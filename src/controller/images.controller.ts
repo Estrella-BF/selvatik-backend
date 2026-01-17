692f798fad5bc4de2e537d9b186b7c257de0b52c
Estrella-BF <gracielabenitesf@gmail.com>
2026-01-17 16:37:21 -0500

    log

--- src/controller/images.controller.ts
+++ src/controller/images.controller.ts
@@ -13,6 +13,7 @@ export const getFolderImagesController = async (request: Request, response: Resp
 
     return response.json(result.resources);
   } catch (err: any) {
+    console.log('---ERROR FOLDER IMAGE:', err)
     return response.status(500).json({ error: err.message });
   }
 };
